afec6234570a42c6e79a69e4462d484734d623cc
Dinhvuquang <91068001+Dinhvuquang@users.noreply.github.com>
2024-08-31 03:31:47 +0700

    Update FirebaseConfig.js

--- ai-travel-planner-app/configs/FirebaseConfig.js
+++ ai-travel-planner-app/configs/FirebaseConfig.js
@@ -7,14 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCT_H542nsGhdYd_9A9glE1gpClQrtrwOY",
-  authDomain: "ai-travel-planner-166ca.firebaseapp.com",
-  projectId: "ai-travel-planner-166ca",
-  storageBucket: "ai-travel-planner-166ca.appspot.com",
-  messagingSenderId: "470394106045",
-  appId: "1:470394106045:web:e0260917793dae65ca87d4",
-  measurementId: "G-TCZ04MW11E"
+
 };
 
 // Initialize Firebase
